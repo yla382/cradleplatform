08f524296bf570175584e49c8223d135e5601a7e
yla382 <yoonhong_lee@sfu.ca>
2019-09-25 20:47:26 -0700

    implementation finished need further testing

---
+++ src/main/java/com/example/TeamMercuryCradlePlatform/Repository/ReadingRepository.java
@@ -0,0 +1,8 @@
+package com.example.TeamMercuryCradlePlatform.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReadingRepository extends CrudRepository {
+}
