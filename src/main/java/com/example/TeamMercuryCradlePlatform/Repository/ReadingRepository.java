e095c3867bbf005038fd46b6abbe69575486a8eb
yla382 <yoonhong_lee@sfu.ca>
2019-09-25 21:08:19 -0700

    Reading repository fixed

--- src/main/java/com/example/TeamMercuryCradlePlatform/Repository/ReadingRepository.java
+++ src/main/java/com/example/TeamMercuryCradlePlatform/Repository/ReadingRepository.java
@@ -1,8 +1,9 @@
 package com.example.TeamMercuryCradlePlatform.Repository;
 
+import com.example.TeamMercuryCradlePlatform.Model.Reading;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReadingRepository extends CrudRepository {
+public interface ReadingRepository extends CrudRepository<Reading, Long> {
 }
