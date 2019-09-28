e6b76926ca070bcabec0e596e31ebad0316f055a
Mashuque Hasan <hasan@sfu.ca>
2019-09-28 11:23:38 -0700

    remove user repository commented out code

--- src/main/java/com/mercury/TeamMercuryCradlePlatform/Repository/UserRepository.java
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/Repository/UserRepository.java
@@ -11,8 +11,4 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByUserId(Integer userid);
     public List<User> findAll();
-//    @Modifying
-//    @Query
-//            ("update user u set u.first_name = :firstName, u.last_name = :lastName, u.email = :email, u.password = :password, u.roles = :roles where u.user_id = id")
-//    public User updateUser(Integer id, User user);
 }
