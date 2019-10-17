81ff0af5aba03b8858d7af0bb07e928a6f42a4d3
yla382 <yoonhong_lee@sfu.ca>
2019-10-17 09:45:56 -0700

    maven setting changed again

--- src/main/java/com/mercury/TeamMercuryCradlePlatform/TeamMercuryCradlePlatformApplication.java
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/TeamMercuryCradlePlatformApplication.java
@@ -1,9 +1,10 @@
 package com.mercury.TeamMercuryCradlePlatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeamMercuryCradlePlatformApplication {
+public class TeamMercuryCradlePlatformApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TeamMercuryCradlePlatformApplication.class, args);
 	}
