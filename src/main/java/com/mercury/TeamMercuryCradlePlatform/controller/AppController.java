7e667add1e600302b30f9f37fc9e18b16e7320ea
ecooke <ecooke@sfu.ca>
2019-10-20 02:40:08 -0700

    Add test function for sending user data to android

--- src/main/java/com/mercury/TeamMercuryCradlePlatform/controller/AppController.java
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/controller/AppController.java
@@ -1,20 +1,38 @@
 package com.mercury.TeamMercuryCradlePlatform.controller;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mercury.TeamMercuryCradlePlatform.model.User;
 import com.mercury.TeamMercuryCradlePlatform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AppController {
     @Autowired
     private UserRepository userRepository;
 
+    // Test function for sending user over
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String sendUser(@PathVariable("id") Integer id) throws JsonProcessingException {
+        //User user = new User("1234", "Earl", "Cooke", "ecooke@sfu.ca", "ADMIN");
+        User user = userRepository.findByUserId(id);
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            return mapper.writeValueAsString(user);
+        }
+        catch (JsonGenerationException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     @RequestMapping("/")
     public String welcome() {
