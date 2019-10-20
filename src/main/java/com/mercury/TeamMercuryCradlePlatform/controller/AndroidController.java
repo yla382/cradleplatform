77839103c7bd28778f02f19697bbf62948cc5708
ecooke <earl_cooke@sfu.ca>
2019-10-20 14:19:42 -0700

    Create function to return all patients to Android app

---
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/controller/AndroidController.java
@@ -0,0 +1,42 @@
+package com.mercury.TeamMercuryCradlePlatform.controller;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercury.TeamMercuryCradlePlatform.model.Patient;
+import com.mercury.TeamMercuryCradlePlatform.model.User;
+import com.mercury.TeamMercuryCradlePlatform.repository.PatientRepository;
+import com.mercury.TeamMercuryCradlePlatform.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/android")
+public class AndroidController {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @RequestMapping(value = "/patients", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getAllPatients() throws JsonProcessingException {
+        List<Patient> patients = patientRepository.findAll();
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(patients);
+        }
+        catch (JsonGenerationException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
