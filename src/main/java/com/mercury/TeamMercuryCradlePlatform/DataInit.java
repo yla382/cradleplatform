9aa449453059c809d9638520d1e4d0f90f30f0f6
qinxinx <qinxin_xu@sfu.ca>
2019-10-23 23:29:45 -0700

    Update DataInit.java

--- src/main/java/com/mercury/TeamMercuryCradlePlatform/DataInit.java
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/DataInit.java
@@ -34,9 +34,9 @@ public class DataInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        User admin = new User(passwordEncoder.encode("1234"), "John", "Lee", "test@test.com", "ADMIN");
-        User vht = new User(passwordEncoder.encode("1234"), "Yoon", "Lee", "test2@test.com","VHT,ADMIN,HEALTHWORKER");
-        User healthWorker = new User(passwordEncoder.encode("1234"), "Megan","Fox", "test3@test.com", "ADMIN,HEALTHWORKER");
+        User admin = new User(passwordEncoder.encode("1234"), "John", "Lee", "test@test.com", "ADMIN", "");
+        User vht = new User(passwordEncoder.encode("1234"), "Yoon", "Lee", "test2@test.com","VHT,ADMIN,HEALTHWORKER", "");
+        User healthWorker = new User(passwordEncoder.encode("1234"), "Megan","Fox", "test3@test.com", "ADMIN,HEALTHWORKER", "");
 
         List<User> users = Arrays.asList(admin, vht, healthWorker);
         userRepository.saveAll(users);
