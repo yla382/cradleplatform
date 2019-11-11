a368e7ad9208326340f921c0bd87fe320f8a50e1
ccervant <ccervant@sfu.ca>
2019-11-11 13:04:44 -0800

    Update data init with new readings

--- src/main/java/com/mercury/TeamMercuryCradlePlatform/DataInit.java
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/DataInit.java
@@ -60,48 +60,111 @@ public class DataInit implements CommandLineRunner {
         Patient patient9 = new Patient("56782458742","Jennifer", "Eriyo", "Uganda", "Tororo", 56);
         Patient patient10 = new Patient("10090277812","Jeje", "Kony", "Uganda", "Sironko", 26);
         Patient patient11 = new Patient("63694282111","Kahinda", "Otafiire", "Uganda", "Lira", 26);
-        Patient patient12 = new Patient("70592244776","Kamanda", "Kauzya", "Uganda", "Lira", 24);
 
         List<Patient> patients = Arrays.asList(patient1, patient2, patient3, patient4, patient5, patient6, patient7,
-                patient8, patient9, patient10, patient11, patient12);
+                patient8, patient9, patient10, patient11);
 
-        Reading reading = new Reading("Abbo", "Kakooza", 29,
-                Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION, "dead"),
+        Reading reading1 = new Reading("Abbo", "Kakooza", 29,
+                Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION, "Shaking"),
                 GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "1",
                 130, 100, 90,
                 ZonedDateTime.of(LocalDate.of( 2019,4,5), LocalTime.of(9,30), ZoneId.systemDefault()));
 
         Reading reading2 = new Reading("Abbo", "Kakooza", 29,
                 Collections.singletonList(Strings.SYMPTOM_NO_SYMPTOMS),
-                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_WEEKS, "7",
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "2",
                 90, 60, 60,
                 ZonedDateTime.of(LocalDate.of( 2019,5,30), LocalTime.of(20,45), ZoneId.systemDefault()));
 
         Reading reading3 = new Reading("Abbo", "Kakooza", 29,
-                Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION, "not dead"),
-                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_NONE, "1",
+                Arrays.asList(Strings.SYMPTOM_HEADACHE),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "3",
                 80, 70, 120,
                 ZonedDateTime.of(LocalDate.of( 2019,6,1), LocalTime.of(3,15), ZoneId.systemDefault()));
 
         Reading reading4 = new Reading("Abbo", "Kakooza", 29,
-                Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION),
-                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_NONE, "1",
-                200, 150, 180,
+                Arrays.asList(Strings.SYMPTOM_HEADACHE),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "4",
+                118, 75, 100,
                 ZonedDateTime.of(LocalDate.of( 2019,7,5), LocalTime.of(19,30), ZoneId.systemDefault()));
 
         Reading reading5 = new Reading("Achen", "Kakooza", 36,
                 Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION),
-                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_NONE, "1",
-                200, 150, 180,
-                ZonedDateTime.of(LocalDate.of( 2019,8,5), LocalTime.of(19,30), ZoneId.systemDefault()));
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_NONE, "0",
+                99, 60, 99,
+                ZonedDateTime.of(LocalDate.of( 2019,7,16), LocalTime.of(7,45), ZoneId.systemDefault()));
 
-        List<Reading> readings = Arrays.asList(reading, reading2, reading3, reading4, reading5);
-        patient1.addAllReadings(readings);
-        patient2.addAllReadings(readings);
-        readings.forEach(r -> r.setPatient(patient1));
+        Reading reading6 = new Reading("Achen", "Kakooza", 36,
+                Arrays.asList("Back Pain", Strings.SYMPTOM_BLEEDING),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_WEEKS, "2",
+                178, 150, 127,
+                ZonedDateTime.of(LocalDate.of( 2019,8,2), LocalTime.of(10,23), ZoneId.systemDefault()));
+
+        Reading reading7 = new Reading("Kizza", "Alupo", 76,
+                Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "8",
+                170, 127, 139,
+                ZonedDateTime.of(LocalDate.of( 2019,6,1), LocalTime.of(4,0), ZoneId.systemDefault()));
+
+        Reading reading8 = new Reading("Jeje", "Kony", 26,
+                Arrays.asList(Strings.SYMPTOM_ABDOMINAL_PAIN, "Leg Pain"),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "3",
+                150, 89, 100,
+                ZonedDateTime.of(LocalDate.of( 2019,2,23), LocalTime.of(5,9), ZoneId.systemDefault()));
+
+        Reading reading9 = new Reading("Kahinda", "Otafiire", 26,
+                Arrays.asList(Strings.SYMPTOM_HEADACHE, Strings.SYMPTOM_BLURRED_VISION),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "7",
+                180, 130, 98,
+                ZonedDateTime.of(LocalDate.of( 2019,1,12), LocalTime.of(17,49), ZoneId.systemDefault()));
+
+        Reading reading10 = new Reading("Kahinda", "Otafiire", 26,
+                Arrays.asList(Strings.SYMPTOM_UNWELL, Strings.SYMPTOM_FEVERISH),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "8",
+                120, 80, 70,
+                ZonedDateTime.of(LocalDate.of( 2019,2,12), LocalTime.of(15,33), ZoneId.systemDefault()));
+
+        Reading reading11 = new Reading("Natukunda", "Mbira", 22,
+                Arrays.asList(Strings.SYMPTOM_UNWELL, Strings.SYMPTOM_BLURRED_VISION, Strings.SYMPTOM_ABDOMINAL_PAIN),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_NONE, "0",
+                118, 88, 79,
+                ZonedDateTime.of(LocalDate.of( 2019,12,12), LocalTime.of(23,29), ZoneId.systemDefault()));
+
+        Reading reading12 = new Reading("Abbo", "Kakooza", 29,
+                Arrays.asList(Strings.SYMPTOM_BLURRED_VISION),
+                GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS, "5",
+                122, 100, 90,
+                ZonedDateTime.of(LocalDate.of( 2019,8,27), LocalTime.of(11,47), ZoneId.systemDefault()));
+
+        List<Reading> readings1 = Arrays.asList(reading1, reading2, reading3, reading4, reading12);
+        List<Reading> readings2 = Arrays.asList(reading5, reading6);
+        List<Reading> readings6 = Arrays.asList(reading11);
+        List<Reading> readings8 = Arrays.asList(reading7);
+        List<Reading> readings10 = Arrays.asList(reading8);
+        List<Reading> readings11 = Arrays.asList(reading9, reading10);
+
+        patient1.addAllReadings(readings1);
+        patient2.addAllReadings(readings2);
+        patient6.addAllReadings(readings6);
+        patient8.addAllReadings(readings8);
+        patient10.addAllReadings(readings10);
+        patient11.addAllReadings(readings11);
+
+        readings1.forEach(r -> r.setPatient(patient1));
+        readings2.forEach(r -> r.setPatient(patient2));
+        readings6.forEach(r -> r.setPatient(patient6));
+        readings8.forEach(r -> r.setPatient(patient8));
+        readings10.forEach(r -> r.setPatient(patient10));
+        readings11.forEach(r -> r.setPatient(patient11));
 
         patientRepository.saveAll(patients);
-        readingRepository.saveAll(readings);
+
+        readingRepository.saveAll(readings1);
+        readingRepository.saveAll(readings2);
+        readingRepository.saveAll(readings6);
+        readingRepository.saveAll(readings8);
+        readingRepository.saveAll(readings10);
+        readingRepository.saveAll(readings11);
 
     }
 }
