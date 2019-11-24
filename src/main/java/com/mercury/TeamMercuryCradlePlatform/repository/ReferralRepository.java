11d8a717feef8fbf9a320f6c9c8dfac62d3dd03e
Yehor Nabokov <ynabokov@sfu.ca>
2019-11-23 22:01:03 -0800

    Check the navbar & change the way to generate assessment graph's data

--- src/main/java/com/mercury/TeamMercuryCradlePlatform/repository/ReferralRepository.java
+++ src/main/java/com/mercury/TeamMercuryCradlePlatform/repository/ReferralRepository.java
@@ -18,4 +18,9 @@ public interface ReferralRepository extends CrudRepository<Referral, Long> {
             "WHERE ref.date_time_sent >=?1 AND ref.date_time_sent < ?2 AND ref.is_pregnant <> 0",
             nativeQuery = true)
     public Integer findNumberOfReferredPregnantWomenPerMonth(String monthFirstDay, String nextMonthFirstDay);
+
+    @Query( value = "SELECT COUNT(*) FROM referral ref " +
+            "WHERE ref.date_time_sent >=?1 AND ref.date_time_sent < ?2 AND ref.is_pregnant <> 0 AND ref.is_assessed <> 0",
+            nativeQuery = true)
+    public Integer findNumberOfAssessedPregnantWomenPerMonth(String monthFirstDay, String nextMonthFirstDay);
 }
